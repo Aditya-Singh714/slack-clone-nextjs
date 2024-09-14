62e28ee1b6ed69751bacd1d9d7b14a07eab1806d
Aditya-Singh714 <adityajaat0000@gmail.com>
2024-09-14 11:33:16 +0530

    implemented User Information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
-  const { signOut } = useAuthActions();
-
   return (
     <div className="text-emerald-900 font-mono text-3xl">
-      Logged In
-      <Button onClick={() => signOut}>Sign out
-      </Button>
+      <UserButton />
     </div>
   );
 }
